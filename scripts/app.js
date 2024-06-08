041730a2277a713a7f439f20b7efd16ce6487208
brandon-bennington <brandon.bennington@codeimmersives.com>
2024-06-08 14:55:57 -0300

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+function sayHello(){
+    console.log('Hello There');
+    return;
+}
+
+function sayGoodbye()
+{
+    console.log('Goodbye');
+}
+
+function init(){
+    console.log("hello world");
+    
+}
+
